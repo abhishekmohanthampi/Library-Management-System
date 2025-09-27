e612bc124978132d8a3d0b63e5cae9282900b3f8
Abhishek Mohan Thampi <134160412+abhishekmohanthampi@users.noreply.github.com>
2025-09-27 22:59:40 +0530

    Add files via upload
    added a basic login and home page seperate for admin and user

---
+++ script.js
@@ -0,0 +1,32 @@
+function showForm(person){
+    document.getElementById("userform").style.display='none';
+    document.getElementById("adminform").style.display='none';
+    if(person=='user'){
+        document.getElementById("userform").style.display='block';        
+    }
+    else{
+        document.getElementById("adminform").style.display='block';
+}}
+function login(person){
+    if(person==='user'){
+    const useremail=document.getElementById('useremail').value;
+    const password=document.getElementById('password').value;
+    if(useremail==="user@123.com" && password==="1234"){
+        window.location.href="home.html";
+    }
+    else{
+        alert("Invalid User Credentials!");
+    }
+    return false;}
+    else{
+    const adminemail=document.getElementById('adminemail').value;
+    const password=document.getElementById('password').value;
+    if(adminemail==="admin@123.com" && password==="1234"){
+        window.location.href="adminhome.html";
+    }
+    else{
+        alert("Invalid Admin Credentials!");
+
+    }
+    return false;
+}}
